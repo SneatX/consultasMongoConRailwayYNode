186db3f5cc8ae873fa30c87c371f4aa78ac7a2f8
SneatX <santi02005@hotmail.com>
2024-07-04 17:13:58 -0500

    feat: :construction: Primeras 4 consultas

--- js/module/movies.js
+++ js/module/movies.js
@@ -11,5 +11,33 @@ export class movies extends connect {
         return this;
     }
 
+    async getAllMovies(){
+        let res = await this.collection.find({}).toArray();
+        await this.conexion.close()
+        return res;
+    }
 
+    async getAllActionMovies(){
+        let res = await this.collection.find({"genre":{$elemMatch:{$eq:"Accion"}}},{projection: {_id:0, name:1, genre:1}}).toArray();
+        await this.conexion.close()
+        return res;
+    }
+
+    async getAllMoviesWithMoreOf200Bluray(){
+        let res = await this.collection.find({format:{$elemMatch:{name:"Bluray", copies: {$gte:200}}}},{projection : {name :1, format: 1}}).toArray();
+        await this.conexion.close()
+        return res;
+    }
+
+    async getAllMoviesWithMinusOf10Dvd(){
+        let res = await this.collection.find({format:{$elemMatch:{name:"dvd", copies: {$lt:10}}}},{projection : {_id:0, name:1, format:1}}).toArray();
+        await this.conexion.close()
+        return res;
+    }
+
+    async getAllMoviesWithCobb(){
+        let res = await this.collection.find({character: {$elemMatch:{apodo: "Cobb"}}},{_id:0, name:1, character:1}).toArray();
+        await this.conexion.close()
+        return res;
+    }
 }
--- main.js
+++ main.js
@@ -2,4 +2,8 @@ import {connect} from './helpers/db/connect.js';
 import { movies } from './js/module/movies.js';
 
 let mongo = new movies()
-
+//console.table(await mongo.getAllMovies());
+//console.table(await mongo.getAllActionMovies());
+//console.table(await mongo.getAllMoviesWithMoreOf200Bluray());
+//console.table(await mongo.getAllMoviesWithMinusOf10Dvd());
+//console.table(await mongo.getAllMoviesWithCobb());
